2aef6b3f4dae6df8e4e2033124fad436a4c503c5
Barry <nobodyuse2000@outlook.com>
2021-08-10 13:54:15 +0800

    Remove the comment

--- Sample/Test2.py
+++ Sample/Test2.py
@@ -17,9 +17,7 @@ data = [x for x in range(1, 4) if x > 0]
 print(data)
 
 # this is a testing for branch
-# dfgsdjkfdsjkfsdkj
-# this dfdfdsff
-# fdfd
+
 mike = Person('Mike', 5)  # 帶進去的參數Mike與5分辨對應__init__裡的name與age
 john = Person('John', 10)
 
