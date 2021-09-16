07c049824903c1b6e752a52b933579695ea9f677
Barry Fan <Barryfan2000@outlook.com>
2021-09-16 16:11:07 +0800

    Add mainform for jenkins

---
+++ Sample/MainForm.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+
+window.title('window')
+
+window.geometry('500x100')
+
+lbl_1 = tk.Label(window, text='This is a testing', bg='yellow', fg='#263238', font=('Arial', 18))
+
+lbl_1.grid(column=0, row=0)
+
+window.mainloop()
